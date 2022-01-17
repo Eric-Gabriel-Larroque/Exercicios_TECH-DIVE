ff55274196d376c1989f18033e7379d0628fda08
Eric-Gabriel-Larroque <ericgabriellarroque@gmail.com>
2022-01-17 05:24:10 -0300

    criando classe para disparar os audios de acordo com o contexto da aventura

---
+++ Modulo_1/Semana_8/Exercicios/Batalha_Final/src/batalhafinal/util/Audio.java
@@ -0,0 +1,38 @@
+package batalhafinal.util;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Audio {
+
+  private static boolean isRunning = false;
+  private static List<Clip> listaDeReproducao = new ArrayList<>();
+
+    public static void tocarAudio(String audioCaminho) {
+
+        try {
+            AudioInputStream audio = AudioSystem.getAudioInputStream(new File(audioCaminho)
+                    .getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            if(isRunning) {
+                listaDeReproducao.get(0).stop();
+                listaDeReproducao.get(0).close();
+                listaDeReproducao.remove(0);
+                isRunning = false;
+            } else {
+                listaDeReproducao.add(clip);
+            }
+            clip.open(audio);
+            clip.start();
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+            isRunning = true;
+        }catch (Exception e) {
+            System.err.printf("Falha ao executar o audio. %nErro: %s ",e.getMessage());
+        }
+    }
+
+}
