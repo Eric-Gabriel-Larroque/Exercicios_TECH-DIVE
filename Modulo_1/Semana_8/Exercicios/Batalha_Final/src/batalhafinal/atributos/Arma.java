d493c2fed53473564dbb5595ce0fbfa2f30af22e
Eric-Gabriel-Larroque <ericgabriellarroque@gmail.com>
2022-01-17 05:20:14 -0300

    criando enums para definir a motivacao e arma de acordo com a classe do personagem

---
+++ Modulo_1/Semana_8/Exercicios/Batalha_Final/src/batalhafinal/atributos/Arma.java
@@ -0,0 +1,33 @@
+package batalhafinal.atributos;
+
+public enum Arma {
+
+    ESPADA("espada",5),
+    MACHADO("machado",5),
+    MARTELO("martelo",5),
+    CLAVA("clava",5),
+    ARCO("arco",5),
+    BESTA("besta",5),
+    CAJADO("cajado",5),
+    LIVRODEFEITICOS("livro de feitiços",5),
+    ARMADILHA("Armadilha",2),
+    MACHADODUPLO("machado duplo",7);
+
+
+    private String arma;
+    private int pontosDeAtaque;
+
+    Arma(String arma,int pontosDeAtaque) {
+
+        this.arma = arma;
+        this.pontosDeAtaque = pontosDeAtaque;
+    }
+
+    public String getArma() {
+        return arma;
+    }
+
+    public int getPontosDeAtaque(){
+        return pontosDeAtaque;
+    }
+}
