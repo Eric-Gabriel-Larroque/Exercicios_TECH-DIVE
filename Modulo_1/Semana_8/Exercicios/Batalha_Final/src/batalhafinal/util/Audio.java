45282ecaca5be52936e9a8408e90fb848e22276c
Eric-Gabriel-Larroque <ericgabriellarroque@gmail.com>
2022-01-19 05:29:36 -0300

    criando variavel para definir loop do audio

--- Modulo_1/Semana_8/Exercicios/Batalha_Final/src/batalhafinal/util/Audio.java
+++ Modulo_1/Semana_8/Exercicios/Batalha_Final/src/batalhafinal/util/Audio.java
@@ -12,25 +12,29 @@ public class Audio {
   private static boolean isRunning = false;
   private static List<Clip> listaDeReproducao = new ArrayList<>();
 
-    public static void tocarAudio(String audioCaminho) {
+    public static void tocarAudio(String audioCaminho, boolean loop) {
 
         try {
             AudioInputStream audio = AudioSystem.getAudioInputStream(new File(audioCaminho)
                     .getAbsoluteFile());
             Clip clip = AudioSystem.getClip();
-            if(isRunning) {
+            if(isRunning&&loop) {
                 listaDeReproducao.get(0).stop();
                 listaDeReproducao.get(0).close();
                 listaDeReproducao.remove(0);
                 isRunning = false;
                 listaDeReproducao.add(clip);
             } else {
-                listaDeReproducao.add(clip);
+                if(loop) {
+                    listaDeReproducao.add(clip);
+                }
             }
             clip.open(audio);
             clip.start();
-            clip.loop(Clip.LOOP_CONTINUOUSLY);
-            isRunning = true;
+            if(loop) {
+                clip.loop(Clip.LOOP_CONTINUOUSLY);
+                isRunning = true;
+            }
         }catch (Exception e) {
             System.err.printf("Falha ao executar o audio. %nErro: %s ",e.getMessage());
         }
