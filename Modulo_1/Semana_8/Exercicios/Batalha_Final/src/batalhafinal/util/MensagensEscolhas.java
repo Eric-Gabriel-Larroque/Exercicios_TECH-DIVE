643b22aa7915b865ae7fc7b78564baf4cea74699
Eric-Gabriel-Larroque <ericgabriellarroque@gmail.com>
2022-01-19 04:16:47 -0300

    criando classe para armazenar as mensagens de multipla escolha disparadas pelo usuario

---
+++ Modulo_1/Semana_8/Exercicios/Batalha_Final/src/batalhafinal/util/MensagensEscolhas.java
@@ -0,0 +1,119 @@
+package batalhafinal.util;
+
+public class MensagensEscolhas {
+
+    public static String[] mensagensMotivacaoVinganca = {
+            "Imagens daquela noite trágica invadem sua mente.\n"+
+            "Você nem precisa se esforçar para lembrar,\n"+
+            "pois essas memórias estão sempre presentes,\n"+
+            "mesmo que de pano de fundo, quando você\n"+
+            "tem outros pensamentos em foco, elas nunca o deixaram.\n",
+
+            "Elas são o combustível que te fizeram chegar até aqui.\n"+
+            "E você sabe que não irá desistir até ter vingado a morte\n"+
+            "daqueles que foram - e pra sempre serão -\n"+
+            "sua fonte de amor e desejo de continuar vivo.\n",
+
+            "O maldito líder finalmente pagará por tanto mal causado\n"+
+            " na vida de tantos"+
+            "(e principalmente na sua)."
+    };
+
+    public static String[] mensagensMotivacaoGloria = {
+            "Você já consegue visualizar na sua mente o povo da cidade\n"+
+            "te recebendo de braços abertos, bardos criando canções\n"+
+            "sobre seus feitos heróicos, nobres te presenteando\n"+
+            "com jóias e diversas riquezas,taberneiros se\n"+
+            "recusando a cobrar por suas bebedeiras e comilanças.\n",
+
+            "Desde já, você sente o amor do público, te louvando\n"+
+            "a cada passo que dá pelas ruas, depois de\n"+
+            "destruir o vilão que tanto assombrou a paz de todos.",
+
+            "Porém, você sabe que ainda falta o último ato dessa história.\n"+
+            "Você se concentra na missão.",
+
+            "A glória o aguarda, mas não antes da última batalha."
+    };
+
+    public static String[] mensagensDesistir = {
+            "O medo invade o seu coração e você sente que ainda não está à altura do desafio.\n"+
+            "Você se volta para a noite lá fora, e corre em direção à segurança."
+    };
+
+    public static String[] mensagensContinuar = {
+            "Você caminha, atento a todos os seus sentidos, por vários metros,\n"+
+            "até visualizar a frente uma fonte de luz, que você imagina ser\n"+
+            "a chama de uma tocha,vindo de dentro de uma porta aberta.",
+
+            "Você se pergunta se dentro dessa sala pode haver inimigos,\n"+
+            "ou alguma armadilha, e pondera sobre como passar pela porta.",
+    };
+
+    public static String[] mensagensPassarSaltando = {
+            "Você se concentra e pula em direção à luz,"+
+            "\nsaltando de antes da porta até o interior da sala.",
+    };
+
+    public static String[] mensagensPassarCorrendo = {
+            "Você respira fundo e desata a correr em direção à sala.",
+
+            "Quando passa pela porta, sente que pisou em uma pedra solta,\n"+
+            "mas não dá muita importância e segue para dentro da sala,\n"+
+            "olhando ao redor à procura de inimigos.",
+
+            "Não tem ninguém, mas você ouve sons de flechas\n"+
+            "batendo na pedra atrás de você,e quando se vira,\n"+
+            "vê várias flechas no chão.",
+
+            "Espiando pela porta, você entende que pisou em uma armadilha\n"+
+            "que soltou flechas de uma escotilha aberta no teto,\n"+
+            "mas por sorte você entrou correndo e conseguiu escapar desse ataque surpresa.",
+    };
+
+    public static String[] mensagensPassarDevagar = {
+            "Você toma cuidado e vai caminhando vagarosamente em direção à luz.",
+
+            "Quando você pisa exatamente embaixo da porta, você sente o chão ceder levemente,\n"+
+            "como se tivesse pisado em uma pedra solta.\n",
+
+            "Você ouve um ruído de mecanismos se movimentando,\n"+
+            "e uma escotilha se abre no teto atrás de você, no corredor.\n",
+
+            "Flechas voam da escotilha em sua direção,\n"+
+            "e você salta para dentro da sala,\n"+
+            "porém uma delas te acerta na perna."
+    };
+
+    public static String[] mensagensPegarArmadura = {
+            "Você resolve usar os equipamentos do inimigo contra ele,"+
+            "\ne trocar algumas peças suas, que estavam danificadas,"+
+            "\npelas peças de armaduras existentes na sala.\n",
+
+            "De armadura nova, você se sente mais protegido para os desafios à sua frente."
+    };
+
+    public static String[] mensagensNaoPegarArmadura = {
+            "Você decide que não precisa utilizar nada que venha das mãos do inimigo.",
+    };
+
+    public static String[] mensagensBeberPocao = {
+            "Você se sente revigorado para seguir adiante!"
+    };
+
+    public static String[] mensagensNaoBeber = {
+            "Você fica receoso de beber algo produzido pelo inimigo."
+    };
+
+    public static String[] mensagensVitoriaVinganca = {
+            "Você obteve sua vingança. Você se ajoelha e cai no choro,\n"+
+            "invadido por uma sensação de alívio e felicidade. Você se vingou,\n"+
+            "tudo que sempre quis, está feito. Agora você pode seguir sua vida."
+    };
+
+    public static String[] mensagensVitoriaGloria = {
+            "O êxtase em que você se encontra não cabe dentro de si.\n"+
+            " Você se ajoelha e grita de alegria.\n"+
+            "A glória o aguarda, você a conquistou."
+    };
+}
