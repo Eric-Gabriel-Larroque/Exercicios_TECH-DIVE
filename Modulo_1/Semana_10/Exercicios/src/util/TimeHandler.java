1ba45ba3e3a9af40e2bd69d2473e36e73bfce1a6
Eric-Gabriel-Larroque <ericgabriellarroque@gmail.com>
2022-01-26 02:55:30 -0300

    criando classe para validar, formatar e criar formatos de horário validos

---
+++ Modulo_1/Semana_10/Exercicios/src/util/TimeHandler.java
@@ -0,0 +1,37 @@
+package util;
+
+import javax.swing.*;
+import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
+
+public class TimeHandler {
+
+    public static boolean horarioValido(String horario) {
+        boolean repetir = true;
+        LocalTime horaFormatada = LocalTime.now();
+        while(repetir) {
+            try {
+                horaFormatada = LocalTime.parse(horario);
+                repetir = false;
+            } catch (DateTimeParseException | NullPointerException dateTimeParseException) {
+                JOptionPane.showMessageDialog(null,
+                        "Formato de horário errado ou fora do intervalo de tempo permitido.");
+                return false;
+            }
+        }
+        return horaFormatada.equals(LocalTime.parse(horario));
+    }
+
+    public static LocalTime setHorario(String mensagem) {
+        String horario;
+        do {
+            horario = JOptionPane.showInputDialog(null,
+                    mensagem);
+        }  while(!horarioValido(horario));
+        return formatar(horario);
+    }
+
+    public static LocalTime formatar(String horario) {
+        return LocalTime.parse(horario);
+    }
+}
