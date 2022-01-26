aff4bc3379bef0b55e77435e78852fed297d54c2
Eric-Gabriel-Larroque <ericgabriellarroque@gmail.com>
2022-01-26 04:55:06 -0300

    implementando metodos para validar entrada de dados para mes e ano

--- Modulo_1/Semana_10/Exercicios/src/util/DataHandler.java
+++ Modulo_1/Semana_10/Exercicios/src/util/DataHandler.java
@@ -1,9 +1,13 @@
 package util;
 
 import javax.swing.*;
+import java.time.DateTimeException;
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.Year;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.util.Arrays;
 
 public class DataHandler {
 
@@ -45,4 +49,36 @@ public class DataHandler {
         return formatar(data);
     }
 
+    public static Month setMes() {
+        int numeroDoMes = 0;
+        Month mes =  Month.from(LocalDate.now());
+        do {
+            try {
+                numeroDoMes = Integer.parseInt(JOptionPane.showInputDialog(null,
+                        "Insira um número de um mês (de 1 a 12):"));
+                mes = Month.of(numeroDoMes);
+            }catch (DateTimeException | NumberFormatException e) {
+                JOptionPane.showMessageDialog(null,
+                        "Tipo de dado inválido ou vazio. Somente números de 1 a 12 são aceitos.",
+                        "Dados inválidos",JOptionPane.ERROR_MESSAGE);
+            }
+        }while(!(numeroDoMes>=1&&numeroDoMes<=12));
+        return mes;
+    }
+
+    public static Year setAno(int anoMinimo, int anoLimite) {
+        boolean repetir = true;
+        Year ano = Year.now();
+        while(repetir) {
+            try {
+                ano = Year.parse(JOptionPane.showInputDialog(null,
+                "Agora, insira um ano válido (formato xxxx)"));
+                repetir = false;
+            } catch (DateTimeParseException | NullPointerException dateTimeParseException) {
+                JOptionPane.showMessageDialog(null,
+                        "Entrada de dados vazio ou ano fora do intervalo de tempo permitido.");
+            }
+        }
+        return ano;
+    }
 }
