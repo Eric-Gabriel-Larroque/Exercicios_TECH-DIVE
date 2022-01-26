81bdb68e5bda54f7985756c0b458a0763e8fea7e
Eric-Gabriel-Larroque <ericgabriellarroque@gmail.com>
2022-01-26 02:54:57 -0300

    criando classe para validar, formatar e criar formatos de data válidos

---
+++ Modulo_1/Semana_10/Exercicios/src/util/DataHandler.java
@@ -0,0 +1,43 @@
+package util;
+
+import javax.swing.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class DataHandler {
+
+    public static boolean dataValida(String data,LocalDate dataMinima,LocalDate datalimite) {
+        boolean repetir = true;
+        LocalDate dataFormatada = LocalDate.now();
+        while(repetir) {
+            try {
+                dataFormatada = LocalDate.parse(data, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+                repetir = false;
+            } catch (DateTimeParseException | NullPointerException dateTimeParseException) {
+                JOptionPane.showMessageDialog(null,
+                        "Formato de data errada ou fora do intervalo de tempo permitido.");
+                return false;
+            }
+        }
+        return dataFormatada.isBefore(datalimite.plusDays(1)) && dataFormatada.isAfter(dataMinima.minusDays(1));
+    }
+
+    public static LocalDate formatar(String data) {
+        return LocalDate.parse(data,DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+    }
+
+    public static String formatar(LocalDate data) {
+            return data.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+    }
+
+    public static LocalDate setData(String mensagem, LocalDate dataMinima,LocalDate dataLimite) {
+        String data;
+        do {
+            data = JOptionPane.showInputDialog(null,
+                    mensagem);
+        }  while(!dataValida(data,dataMinima,dataLimite));
+        return formatar(data);
+    }
+
+}
