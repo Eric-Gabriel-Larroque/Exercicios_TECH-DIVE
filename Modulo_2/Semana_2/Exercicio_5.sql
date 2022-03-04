c90fbfbc6c09e9ce5d90ded03a4b46fec52bda48
Eric-Gabriel-Larroque <ericgabriellarroque@gmail.com>
2022-03-04 19:06:50 -0300

    Exercitando o TRUNCATE e o DROP

---
+++ Modulo_2/Semana_2/Exercicio_5.sql
@@ -0,0 +1,13 @@
+TRUNCATE TABLE university.alunos;
+TRUNCATE TABLE university.cursos;
+TRUNCATE TABLE university.departamentos;
+TRUNCATE TABLE university.disciplinas;
+TRUNCATE TABLE university.matriculas;
+TRUNCATE TABLE university.matrizes_cursos;
+
+DROP TABLE university.alunos;
+DROP TABLE university.cursos;
+DROP TABLE university.departamentos;
+DROP TABLE university.disciplinas;
+DROP TABLE university.matriculas;
+DROP TABLE university.matrizes_cursos;
