2d5ed21f3ff7e2cf50efb319bf12a7caf0f26024
Eric-Gabriel-Larroque <ericgabriellarroque@gmail.com>
2022-03-14 16:14:08 -0300

    Inserindo metodos de interacao com usuario, escolha da entidade e selecao do metodo a ser executado

--- Modulo_2/Semana_3/Exercicios/src/mercado/util/Interacao.java
+++ Modulo_2/Semana_3/Exercicios/src/mercado/util/Interacao.java
@@ -1,4 +1,14 @@
 package mercado.util;
+import mercado.models.dao.ClassificacaoDAO;
+import mercado.models.dao.MarcaDAO;
+import mercado.models.dao.ProdutoDAO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static mercado.util.Validacao.*;
 public class Interacao {
 
@@ -17,5 +27,64 @@ public class Interacao {
         }
 
         return dadosPessoais;
+    }
+
+    public static int escolherOpcao(String mensagem, Object... opcoesPossiveis) {
+        boolean repetir = true;
+        List<Integer> opcoes = new ArrayList<>();
+        String listarOpcoes = "";
+        int resposta = 0;
+
+        for(int i = 0; i < opcoesPossiveis.length;i++) {
+            opcoes.add(i+1);
+            listarOpcoes+= "\n"+(i+1)+" - "+opcoesPossiveis[i];
+        }
+        while(repetir) {
+            resposta = validaNumeros(mensagem+listarOpcoes+"\n--> ");
+            if(!opcoes.contains(resposta)) {
+                System.err.println("Escolha um número dentre as opções disponíveis.");
+            }else {
+                repetir = false;
+            }
+        }
+        --resposta;
+        return  resposta;
+    }
+
+    public static Object selecionaTabela(Connection conn) throws SQLException {
+        String[] nomeEntidades = {"Produto","Classificacao","Marca","Sair"};
+        Object entidade = null;
+        int entidadeEscolhida = escolherOpcao("Com qual das entidades vai querer trabalhar?",nomeEntidades);
+
+         switch (entidadeEscolhida) {
+             case 0:
+                 entidade = new ProdutoDAO(conn);
+                 break;
+             case 1:
+                 entidade = new ClassificacaoDAO(conn);
+                 break;
+             case 2:
+                 entidade = new MarcaDAO(conn);
+                 break;
+             case 3:
+                 System.out.println("Volte sempre! :)");
+                 System.exit(0);
+                 break;
+        }
+        return entidade;
+    }
+
+    public static int operacoesDAO(Object entidade) {
+        List<String> listaOperacoes = new ArrayList<>();
+        String[] operacoesDAO = {"Listar tabela inteira","Listar pelo Nome","Listar pelo ID","Criar registros","Deletar registros","Voltar"};
+        Collections.addAll(listaOperacoes,operacoesDAO);
+        if(entidade.getClass().getSimpleName().equals("ProdutoDAO")) {
+            listaOperacoes.add(1,"Listar com JOIN");
+        }
+        int operacao = -1;
+        while(operacao!=5) {
+            operacao = escolherOpcao("O que deseja fazer?", listaOperacoes.toArray());
+        }
+        return operacao;
     }
 }
