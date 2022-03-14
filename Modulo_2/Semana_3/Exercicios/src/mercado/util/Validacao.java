8dfb28c7d9af899c1a6b19f306dcdfb00f9e2ca3
Eric-Gabriel-Larroque <ericgabriellarroque@gmail.com>
2022-03-14 16:14:42 -0300

    Criando validacao de Double para insercao do dado na Entidade Produto

--- Modulo_2/Semana_3/Exercicios/src/mercado/util/Validacao.java
+++ Modulo_2/Semana_3/Exercicios/src/mercado/util/Validacao.java
@@ -1,8 +1,5 @@
 package mercado.util;
 
-import javax.swing.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Validacao {
@@ -23,21 +20,22 @@ public class Validacao {
         return entrada;
     }
 
-    public static int escolherOpcao(String mensagem, Object... opcoesPossiveis) {
-
-        List<Integer> opcoes = new ArrayList<>();
-        String listarOpcoes = "";
-        int resposta = 0;
-
-        for(int i = 0; i < opcoesPossiveis.length;i++) {
-            opcoes.add(i+1);
-            listarOpcoes+= "\n"+(i+1)+" - "+opcoesPossiveis[i];
-        }
-        while(!opcoes.contains(resposta)) {
-                resposta = validaNumeros(mensagem+listarOpcoes+"\n--> ");
-        }
-        --resposta;
-        return  resposta;
+    public static double validaDouble(String mensagem){
+        boolean repetir = true;
+        double resposta = 0;
+        do{
+            try{
+                System.out.print(mensagem);
+                resposta =
+                        Double.parseDouble(sc.nextLine());
+                repetir = false;
+            }catch (NumberFormatException numberFormatException) {
+                System.err.println("Somente números são permitidos");
+            } catch (Exception e) {
+                System.err.println("Erro: "+e.getMessage());
+            }
+        }while(repetir);
+        return resposta;
     }
 
     public static int validaNumeros(String mensagem){
