64bfa2faf7d585cf50a0f60718115216160edd4f
Eric-Gabriel-Larroque <ericgabriellarroque@gmail.com>
2022-03-14 16:13:03 -0300

    executando loop na interação do usuario com o console

--- Modulo_2/Semana_3/Exercicios/src/mercado/program/Main.java
+++ Modulo_2/Semana_3/Exercicios/src/mercado/program/Main.java
@@ -1,26 +1,48 @@
 package mercado.program;
 
 import mercado.connection.JdbcConnection;
+import mercado.models.dao.ClassificacaoDAO;
+import mercado.models.dao.MarcaDAO;
 import mercado.models.dao.ProdutoDAO;
+import mercado.models.entitys.Classificacao;
+import mercado.models.entitys.Marca;
+
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-import static mercado.util.Validacao.*;
+import static mercado.util.Interacao.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        String resposta;
-        String[] operacoes = {"Listar tabelas","Criar registros","Atualizar registros","Deletar Registros","Sair"};
         JdbcConnection jdbcConnection = new JdbcConnection();
         try(Connection connection = jdbcConnection.connect()) {
-            System.out.println("Conexão com BD realizada com sucesso!");
             ProdutoDAO produtoDAO = new ProdutoDAO(connection);
-
-            escolherOpcao("Bem-vindo(a)! O que deseja fazer?",operacoes);
+            ClassificacaoDAO classificacaoDAO = new ClassificacaoDAO(connection);
+            MarcaDAO marcaDAO = new MarcaDAO(connection);
+            System.out.println("Conexão com BD realizada com sucesso!");
+            int operacao = -1;
+            Object entidade = new Object();
+            while(entidade!=null) {
+                entidade = selecionaTabela(connection);
+                operacao = operacoesDAO(entidade);
+                switch (entidade.getClass().getSimpleName()){
+                    case "ProdutoDAO" -> produtoDAO.operacoes(operacao);
+                    case "ClassificacaoDAO" -> classificacaoDAO.operacoes(operacao);
+                    case "MarcaDAO" -> marcaDAO.operacoes(operacao);
+                }
+            }
 
         }catch (SQLException e) {
             System.err.println("Não foi possível se conectar ao Banco de Dados. Erro - "+e.getMessage());
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("Erro: "+e.getMessage());
+            e.printStackTrace();
         }
+
     }
 }
