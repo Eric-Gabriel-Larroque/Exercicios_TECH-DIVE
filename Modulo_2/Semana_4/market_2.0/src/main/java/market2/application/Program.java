60378bbe8b4ebfa960bd71d80e050f00686c71f5
Eric-Gabriel-Larroque <ericgabriellarroque@gmail.com>
2022-03-21 13:27:43 -0300

    implementando interacao com o usuario e executando na classe principal

--- Modulo_2/Semana_4/market_2.0/src/main/java/market2/application/Program.java
+++ Modulo_2/Semana_4/market_2.0/src/main/java/market2/application/Program.java
@@ -5,15 +5,10 @@ import market2.util.Input;
 import market2.view.CategoryView;
 import market2.view.ClientView;
 import market2.view.ProductView;
-
 import javax.persistence.EntityManager;
 
-import static market2.util.Input.*;
-
 public class Program {
-
     public static void main(String[] args) {
-
         EntityManager em = new JpaConnectionFactory().getEntityManage();
         int chosenTable = new Input().chooseTable();
         switch (chosenTable) {
@@ -21,6 +16,5 @@ public class Program {
             case 1 -> new CategoryView(em).chooseOperation();
             case 2 -> new ClientView(em).chooseOperation();
         }
-
     }
 }
