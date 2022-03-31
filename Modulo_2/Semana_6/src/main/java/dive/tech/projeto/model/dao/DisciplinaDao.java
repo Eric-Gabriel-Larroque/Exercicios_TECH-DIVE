34c34aa972f90ecf39b018be993e587dbb7aae50
Eric-Gabriel-Larroque <ericgabriellarroque@gmail.com>
2022-03-31 20:07:47 -0300

    iniciando criacao da camada de acesso a dados da entidade disciplina

---
+++ Modulo_2/Semana_6/src/main/java/dive/tech/projeto/model/dao/DisciplinaDao.java
@@ -0,0 +1,15 @@
+package dive.tech.projeto.model.dao;
+
+import dive.tech.projeto.model.entity.Disciplina;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DisciplinaDao {
+
+    public static List<Disciplina> disciplinas = new ArrayList<>();
+
+    public Disciplina createDisciplina(Disciplina disciplina) {
+        return null;
+    }
+}
