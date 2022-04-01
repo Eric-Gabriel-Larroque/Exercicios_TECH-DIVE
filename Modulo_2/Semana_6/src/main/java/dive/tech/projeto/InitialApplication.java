bb743a8134c3e3129de2e327bdefa6bb8e402178
Eric-Gabriel-Larroque <ericgabriellarroque@gmail.com>
2022-04-01 18:37:33 -0300

    criando classe para abrir o endpoint inicial

---
+++ Modulo_2/Semana_6/src/main/java/dive/tech/projeto/InitialApplication.java
@@ -0,0 +1,9 @@
+package dive.tech.projeto;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class InitialApplication extends Application {
+
+}
