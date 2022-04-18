371f06a2b8335f91b3324a705ab3fd9392e80df5
Eric-Gabriel-Larroque <ericgabriellarroque@gmail.com>
2022-04-17 23:43:30 -0300

    criando repository da entidade item simulando as querys realizadas no banco de dados

---
+++ Modulo_2/Semana_8/ListaDeCompras/src/main/java/br/com/techdive/listadecompras/repository/ItemRepository.java
@@ -0,0 +1,37 @@
+package br.com.techdive.listadecompras.repository;
+
+import br.com.techdive.listadecompras.model.Item;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.*;
+
+@ApplicationScoped
+public class ItemRepository {
+
+    private List<Item> itens = new ArrayList<>();
+    private Long idAtual = 0L;
+
+    public ItemRepository() {}
+
+    public List<Item> getItens() {
+        return itens;
+    }
+
+    public void adicionar(Item itemNovo) {
+        itens.add(itemNovo);
+    }
+
+    public void remover(String nomeDoItem) {
+        itens.removeIf(i-> i.getNome().equalsIgnoreCase(nomeDoItem));
+    }
+    public void removerItens(List<Item> itensSelecionados) {
+        for(Item item: itensSelecionados) {
+            itens.removeIf(i-> i.getNome().equalsIgnoreCase(item.getNome()));
+        }
+    }
+
+    public boolean existe(String nomeDoItem) {
+        return itens.stream().anyMatch(i->i.getNome().equalsIgnoreCase(nomeDoItem));
+    }
+
+}
