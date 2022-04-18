70cdd487a9a9fbdc44004d515a39b07528694367
Eric-Gabriel-Larroque <ericgabriellarroque@gmail.com>
2022-04-17 23:42:17 -0300

    criando modelagem da entidade Item

---
+++ Modulo_2/Semana_8/ListaDeCompras/src/main/java/br/com/techdive/listadecompras/model/Item.java
@@ -0,0 +1,48 @@
+package br.com.techdive.listadecompras.model;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.PositiveOrZero;
+
+public class Item {
+
+    @NotEmpty(message = "{attr.nome.nulo}")
+    private String nome;
+
+    @PositiveOrZero(message = "{attr.quantidade.menorQueZero}")
+    private float quantidade;
+
+    @NotEmpty(message = "{attr.medida.nula}")
+    private String medida;
+
+    public Item() {}
+
+
+    @Override
+    public String toString() {
+        return quantidade+" "+medida+"(s) de "+nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome.toLowerCase();
+    }
+
+    public float getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(float quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getMedida() {
+        return medida;
+    }
+
+    public void setMedida(String medida) {
+        this.medida = medida.toLowerCase();
+    }
+}
