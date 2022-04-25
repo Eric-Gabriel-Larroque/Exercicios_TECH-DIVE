86f690da001e85b463a65fca892573c3c6fb94f5
Eric-Gabriel-Larroque <ericgabriellarroque@gmail.com>
2022-04-24 21:43:43 -0300

    criando metodo para cadastro de escola nas camadas de negocios e de servico

--- Modulo_2/Semana_9/m2s7-main/src/main/java/projeto/business/EscolaBusiness.java
+++ Modulo_2/Semana_9/m2s7-main/src/main/java/projeto/business/EscolaBusiness.java
@@ -1,12 +1,111 @@
 package projeto.business;
 
+import org.apache.commons.lang3.StringUtils;
+import projeto.dto.EscolaDTO;
+import projeto.dto.EstudanteDTO;
+import projeto.dto.TurmaDTO;
+import projeto.entity.Endereco;
+import projeto.entity.Escola;
+import projeto.entity.Estudante;
+import projeto.entity.Turma;
+import projeto.exception.BusinessException;
 import projeto.repository.EscolaRepository;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EscolaBusiness {
 
     @Inject
     private EscolaRepository escolaRepository;
 
+    public EscolaDTO consultarDadosEscola(Long idEscola) throws BusinessException {
+        Escola escola = escolaRepository.find(Escola.class, idEscola);
+        if (escola == null) {
+            throw new BusinessException("Escola não encontrada através do ID " + idEscola + ".");
+        }
+
+        return new EscolaDTO(escola);
+    }
+
+    public void cadastrar(EscolaDTO escolaDTO) {
+
+    }
+
+    public void validarCadastrar(EscolaDTO escolaDTO) throws BusinessException {
+        List<String> erros = new ArrayList<>();
+
+        if(escolaDTO.getTurmas().isEmpty()) {
+            erros.add("Necessário adicionar pelo menos uma turma.");
+        }
+
+        if(escolaDTO.getIdEndereco() == null) {
+            erros.add("Valor de endereço é inválido.");
+        }
+
+        if(escolaDTO.getCriacao() == null) {
+            erros.add("O valor da data de criação da escola é inválido.");
+        }
+
+        if(StringUtils.isBlank(escolaDTO.getNome())) {
+            erros.add("O valor do nome é inválido.");
+        }
+
+        if(!erros.isEmpty()) {
+            throw new BusinessException(erros);
+        }
+    }
+
+    public void cadastrarEscolaNoBanco(EscolaDTO escolaDTO) throws BusinessException{
+        Escola escola;
+
+        if(escolaDTO.getIdEscola() != null) {
+            escola = escolaRepository.find(Escola.class,escolaDTO.getIdEscola());
+
+            if(escola.getIdEscola() == null) {
+                throw new BusinessException("Escola não encontrada.");
+            }
+        } else {
+            escola = new Escola();
+        }
+
+        escola.setCriacao(escolaDTO.getCriacao());
+        escola.setNome(escolaDTO.getNome());
+
+        Endereco endereco = escolaRepository.find(Endereco.class,escolaDTO.getIdEndereco());
+
+        if(endereco == null) {
+            throw new BusinessException("Endereço não encontrado.");
+        }
+
+        escola.setEndereco(endereco);
+
+        for (Turma turma : escola.getTurmas()) {
+            if (escolaDTO.getTurmas()
+                    .stream()
+                    .noneMatch(turmaDTO -> turmaDTO.getIdTurma().equals(turma.getIdTurma()))) {
+                turma.setEscola(null);
+            }
+        }
+
+        for (TurmaDTO turmaDTO : escolaDTO.getTurmas()) {
+            if (escola.getTurmas()
+                    .stream()
+                    .noneMatch(turma -> turma.getIdTurma().equals(turmaDTO.getIdTurma()))) {
+                Turma turma = escolaRepository.find(Turma.class, turmaDTO.getIdTurma());
+                if (turma == null) {
+                    throw new BusinessException("Turma não encontrada.");
+                }
+                turma.setEscola(escola);
+            }
+        }
+
+        if (escola.getIdEscola() != null) {
+            escolaRepository.merge(escola);
+        } else {
+            escolaRepository.persist(escola);
+            escolaDTO.setIdEscola(escola.getIdEscola());
+        }
+    }
 }
--- Modulo_2/Semana_9/m2s7-main/src/main/java/projeto/service/EscolaService.java
+++ Modulo_2/Semana_9/m2s7-main/src/main/java/projeto/service/EscolaService.java
@@ -1,6 +1,8 @@
 package projeto.service;
 
 import projeto.business.EscolaBusiness;
+import projeto.dto.EscolaDTO;
+import projeto.exception.BusinessException;
 import projeto.repository.EscolaRepository;
 
 import javax.ejb.Stateless;
@@ -15,4 +17,11 @@ public class EscolaService {
     @Inject
     private EscolaRepository escolaRepository;
 
+    public EscolaDTO consultarDadosEscola(Long idEscola) throws BusinessException {
+        return escolaBusiness.consultarDadosEscola(idEscola);
+    }
+
+    public void cadastrar(EscolaDTO escolaDTO) throws BusinessException{
+        escolaBusiness.cadastrar(escolaDTO);
+    }
 }
